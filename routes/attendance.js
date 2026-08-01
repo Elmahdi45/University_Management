8fe95924b6ac184985648cf7a309c78b73b9c947
Elmahdi45 <elmehdikhardi25@gmail.com>
2026-08-01 04:54:03 +0200

    Added attendance controller / business logic/upgraded relationships

--- routes/attendance.js
+++ routes/attendance.js
@@ -7,8 +7,19 @@ const {
 }=require('../middlwares/protectedAuth');
 
 const {
-     
+     createAttendance,
+     getAttendance,
+     getAttendanceByClassAndModule,
+     getMyAttendance,
+     editAttendance,
+     deleteAttendance
      
 }=require('../controllers/attendanceController');
 
+router.post('/',authentificationSecurity,authorize(["Admin","Teacher"]),createAttendance);
+router.get('/',authentificationSecurity,authorize(["Admin","Teacher"]),getAttendance);
+router.get('/class/:class_id/module/:module_id',authentificationSecurity,authorize(["Admin","Teacher"]));
+router.get('/me',authentificationSecurity,authorize(["Admin","Student"]),getMyAttendance);
+router.put('/edit-attendance/:id',authentificationSecurity,authorize(["Admin","Teacher"]),editAttendance);
+router.delete('/delete-attendance/:id',authentificationSecurity,authorize(["Admin","Teacher"]),deleteAttendance);
 module.exports=router;
